52721b0950d70915c5e906e4ab658d171d93433a
palakgupta2345 <palakgupta0421@gmail.com>
2024-10-15 14:45:09 +0530

    day5 code

---
+++ Day5/script.js
@@ -0,0 +1,16 @@
+// let x=confirm("do you want to log out? ");
+// alert(x);
+
+// let v=prompt("enter your name",'Rahul');
+// alert(v);
+
+// let disp=()=>{console.log("Welcome to arrow fuction in js");}; 
+// disp()
+
+// let say=()=>console.log("i am single arrrow fun");
+// say();
+
+// let sum=(x,y)=>{return x+y;}
+// let res=sum(12,32);
+// console.log(res)
+
